57090aac6b12e53affb8a86d29b4198325416f73
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-27 02:43:24 +0800

    init

---
+++ Tumbleweed/YagooNoises/app/src/main/java/com/yuzumin/yagoonoises/Splash.java
@@ -0,0 +1,67 @@
+package com.yuzumin.yagoonoises;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class Splash extends AppCompatActivity {
+
+    boolean isLaunched;
+    ConstraintLayout Layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Layout=findViewById(R.id.layout);
+        isLaunched=false;
+
+
+
+
+        Layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(!isLaunched){
+                    isLaunched=true;
+                    startActivity(new Intent(Splash.this, MainActivity.class));
+                    finish();
+                }
+                return false;
+            }
+        });
+
+
+        Thread timer= new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    //Display for 3 seconds
+                    sleep(1500);
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    if(!isLaunched){
+                        isLaunched=true;
+                        startActivity(new Intent(Splash.this, MainActivity.class));
+                        finish();
+                    }
+                }
+            }
+        };
+        timer.start();
+
+    }
+}
