85142645719f8051ba6afdc413569438746b4a22
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-29 12:40:28 +0800

    Update Splash.java

--- Tumbleweed/YagooNoises/app/src/main/java/com/yuzumin/yagoonoises/Splash.java
+++ Tumbleweed/YagooNoises/app/src/main/java/com/yuzumin/yagoonoises/Splash.java
@@ -1,17 +1,51 @@
 package com.yuzumin.yagoonoises;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
 public class Splash extends AppCompatActivity {
 
     boolean isLaunched;
     ConstraintLayout Layout;
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor activation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +55,10 @@ public class Splash extends AppCompatActivity {
         Layout=findViewById(R.id.layout);
         isLaunched=false;
 
-
+        sharedPref =getSharedPreferences("serverActivation", MODE_PRIVATE);
+        if (!sharedPref.getBoolean("isActivated", false)) {
+            String url = "https://yuzumin.github.io/Hololive-CEO-Yagoo-Noises/V1.json";
+        }
 
 
         Layout.setOnTouchListener(new View.OnTouchListener() {
@@ -62,6 +99,6 @@ public class Splash extends AppCompatActivity {
             }
         };
         timer.start();
-
     }
 }
+
