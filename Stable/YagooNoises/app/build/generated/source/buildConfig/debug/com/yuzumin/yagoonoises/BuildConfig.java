8fd647960ac78e8ef1ca7ec7999b0aca7ef235f9
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-31 00:39:20 +0800

    update

--- Stable/YagooNoises/app/build/generated/source/buildConfig/debug/com/yuzumin/yagoonoises/BuildConfig.java
+++ Stable/YagooNoises/app/build/generated/source/buildConfig/debug/com/yuzumin/yagoonoises/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.yuzumin.yagoonoises";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "2021.8 REV-1.0";
+  public static final String VERSION_NAME = "2021.8 REV-1";
 }
