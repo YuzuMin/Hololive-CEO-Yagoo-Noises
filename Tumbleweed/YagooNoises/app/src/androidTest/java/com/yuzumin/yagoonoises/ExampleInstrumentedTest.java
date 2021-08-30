c45995f08e535a56fe56d98fa51b3e7898e72f5d
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-08-31 00:44:39 +0800

    update

--- Tumbleweed/YagooNoises/app/src/androidTest/java/com/yuzumin/yagoonoises/ExampleInstrumentedTest.java
+++ Tumbleweed/YagooNoises/app/src/androidTest/java/com/yuzumin/yagoonoises/ExampleInstrumentedTest.java
@@ -17,11 +17,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("com.learntodroid.simplealarmclock", appContext.getPackageName());
-    }
+	@Test
+	public void useAppContext() {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+		assertEquals("in.basulabs.shakealarmclock", appContext.getPackageName());
+	}
 }
---
+++ Tumbleweed/YagooNoises/app/src/main/java/com/yuzumin/yagoonoises/alarm/Fragment_AlarmDetails_DatePicker.java
@@ -0,0 +1,54 @@
+package com.yuzumin.yagoonoises.alarm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+
+import com.yuzumin.yagoonoises.R;
+
+public class Fragment_AlarmDetails_DatePicker extends Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.frag_alarmdetails_datepicker, container, false);
+
+		ViewModel_AlarmDetails viewModel = new ViewModelProvider(requireActivity()).get(ViewModel_AlarmDetails.class);
+
+		Calendar minCalendar = Calendar.getInstance();
+		minCalendar.set(Calendar.DAY_OF_MONTH, viewModel.getMinDate().getDayOfMonth());
+		minCalendar.set(Calendar.MONTH, viewModel.getMinDate().getMonthValue() - 1);
+		minCalendar.set(Calendar.YEAR, viewModel.getMinDate().getYear());
+		minCalendar.set(Calendar.HOUR_OF_DAY, 1);
+		minCalendar.set(Calendar.MINUTE, 0);
+		minCalendar.set(Calendar.SECOND, 0);
+
+		DatePicker datePicker = view.findViewById(R.id.datePicker);
+		datePicker.setMinDate(minCalendar.getTimeInMillis());
+
+		datePicker.init(viewModel.getAlarmDateTime().getYear(), viewModel.getAlarmDateTime().getMonthValue() - 1,
+				viewModel.getAlarmDateTime().getDayOfMonth(), (datePicker1, newYear, newMonthOfYear, newDayOfMonth) -> {
+
+					viewModel.setAlarmDateTime(viewModel.getAlarmDateTime().withDayOfMonth(newDayOfMonth));
+					viewModel.setAlarmDateTime(viewModel.getAlarmDateTime().withMonth(newMonthOfYear + 1));
+					viewModel.setAlarmDateTime(viewModel.getAlarmDateTime().withYear(newYear));
+
+					viewModel.setIsChosenDateToday(viewModel.getAlarmDateTime().toLocalDate().equals(LocalDate.now()));
+					viewModel.setHasUserChosenDate(true);
+				});
+
+		return view;
+	}
+
+}
